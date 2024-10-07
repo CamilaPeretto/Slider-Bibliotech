65cbf6cea22486393067606a1a9972de7a556ee1
Camila <camila.peretto@aluno,unip,brgit config --global user.email camila.peretto@aluno.unip.br>
2024-10-07 20:00:07 -0300

    Refatorar funcionalidade do slider na página inicial

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function nextImage() {
   }
   document.getElementById("radio" + count).checked = true;
 }
+// Função para reiniciar o intervalo quando o usuário clica manualmente
+function resetInterval() {
+              clearInterval(interval); // Limpa o intervalo atual
+              interval = setInterval(nextImage, 5000); // Reinicia o intervalo automático
+            }
+            
+            // Adicionando evento de clique para reiniciar o intervalo quando o usuário clicar nas bolinhas manuais
+            document.querySelectorAll('.manual-btn').forEach(btn => {
+              btn.addEventListener('click', resetInterval);
+            });
